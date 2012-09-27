672943aeb384b7bdc1b1c770337a3950aba6a63d
husarz <apilat@gmail.com>
2012-09-27 18:02:05 +0200

    commit text manager

--- MyTest/src/my/test/apps/admin/ui/text/TextManager.java
+++ MyTest/src/my/test/apps/admin/ui/text/TextManager.java
@@ -4,10 +4,13 @@ import my.test.apps.admin.Main.TextFeed;
 import my.test.apps.admin.ui.text.toolbar.RichTextToolbar;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.i18n.client.Constants;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.RichTextArea;
 import com.google.gwt.user.client.ui.Widget;
@@ -27,23 +30,23 @@ public class TextManager extends Composite implements TextFeed{
 //	}
 
 	@UiField HTMLPanel panel;
-//	@UiField RichTextArea area;
-//	@UiField(provided = true) RichTextToolbar bar;
+	@UiField(provided = true) RichTextArea area;
+	@UiField(provided = true) RichTextToolbar bar;
+	@UiField Button add;
+	@UiField HTML label;
 
 	public TextManager() {
+		area = new RichTextArea();
+		bar = new RichTextToolbar(area);
 		initWidget(uiBinder.createAndBindUi(this));
-//		bar = new RichTextToolbar(area);
 //		area.ensureDebugId("cwRichText-area");
-		RichTextArea area = new RichTextArea();
-		area.setSize("100%", "14em");
-		
-		
-		RichTextToolbar bar = new RichTextToolbar(area);
-		bar.setWidth("100%");
-		panel.add(bar);
-		panel.add(area);
 //		toolBar.ensureDebugId("cwRichText-toolbar");
-//		bar.setWidth("90%");
+
+	}
+	
+	@UiHandler("add")
+	void addClick(ClickEvent e){
+		label.setHTML(area.getHTML());
 	}
 
 }
